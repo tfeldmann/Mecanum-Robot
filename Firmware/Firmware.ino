24cb2f09570c872fb2876b81e339c465a6bc02ab
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-05-08 18:49:51 +0200

    Added Can_bus_shield library

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,9 +1,50 @@
-void setup() {
-  // put your setup code here, to run once:
+// demo: CAN-BUS Shield, receive data
+#include <mcp_can.h>
+#include <SPI.h>
 
-}
+unsigned char Flag_Recv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
 
-void loop() {
-  // put your main code here, to run repeatedly: 
+void setup()
+{
+  attachInterrupt(0, MCP2515_ISR, FALLING);     // start interrupt
+  Serial.begin(115200);
   
+  if(CAN.begin(CAN_500KBPS) ==CAN_OK) Serial.print("can init ok!!\r\n");
+  else Serial.print("Can init fail!!\r\n");
 }
+
+void MCP2515_ISR()
+{
+    Flag_Recv = 1;
+}
+
+void loop()
+{
+    if(Flag_Recv)                           // check if get data
+    {
+      Flag_Recv = 0;                        // clear flag
+      CAN.readMsgBuf(&len, buf);            // read data,  len: data length, buf: data buf
+      Serial.println("CAN_BUS GET DATA!");
+      Serial.print("data len = ");
+      Serial.println(len);
+      
+      for(int i = 0; i<len; i++)            // print the data
+      {
+        Serial.print(buf[i]);Serial.print("\t");
+      }
+      Serial.println();
+    }
+    
+  // send data:  id = 0x00, standrad flame, data len = 8, stmp: data buf
+  
+  unsigned char test[2] = "$";
+  CAN.sendMsgBuf(0x00, 0, 2, test);  
+  delay(100);                       // send data per 100ms
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
