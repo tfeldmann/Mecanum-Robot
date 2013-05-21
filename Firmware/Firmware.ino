3ca5058a9a20c554ab530ad24d4cd0530ab61b20
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-05-21 13:48:39 +0200

    removed readCanMessages

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -5,64 +5,14 @@
 // /_/  /_/\___/\___/\__,_/_/ /_/\__,_/_/ /_/ /_/
 //
 
-#include <CAN.h>
-#include <SPI.h>
-
 void setup()
 {
     Serial.begin(115200);
-    api_init();
-    robot_init();
+    api_begin();
+    robot_begin();
 }
 
 void loop()
 {
     api_update();
-    // readCanMessages();
-}
-
-/**
- * Reads all incoming CAN-Messages and dumps them on the serial port
- */
-void readCanMessages()
-{
-    byte length = 0;
-    byte rx_status = 0;
-    unsigned short frame_id = 0;
-    byte frame_data[8];
-
-    // clear frame data
-    for (int i = 0; i < 8; i++)
-    {
-        frame_data[i] = 0;
-    }
-
-    rx_status = CAN.readStatus();
-    if ((rx_status & 0x01) == 0x01)
-    {
-        CAN.readDATA_ff_0(&length, frame_data, &frame_id);
-    }
-    else if ((rx_status & 0x02) == 0x02)
-    {
-        CAN.readDATA_ff_1(&length, frame_data, &frame_id);
-    }
-
-    if (((rx_status & 0x01) == 0x01) || ((rx_status & 0x02) == 0x02))
-    {
-        Serial.print("Rx:");
-        Serial.print(rx_status, HEX);
-        Serial.print(" ");
-        Serial.print(length, HEX);
-
-        Serial.print(" - ");
-        Serial.print(frame_id, HEX);
-        Serial.print(": ");
-
-        for (int i = 0; i < 8; i++)
-        {
-            Serial.print(" ");
-            Serial.print(frame_data[i], HEX);
-        }
-        Serial.println();
-    }
 }
