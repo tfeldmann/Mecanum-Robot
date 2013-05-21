b4beafaec216932b4e1e0fae322453633e787e12
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-05-21 13:04:14 +0200

    simplified the api

--- Firmware/Api.ino
+++ Firmware/Api.ino
@@ -13,25 +13,37 @@ void api_update()
 
 void api_unknown(const char *command)
 {
-    Serial.print("!Unknown command: ");
+    Serial.print("!E02: Unknown command: ");
     Serial.println(command);
 }
 
 void api_init()
 {
-    serialCommand.addCommand("v", api_v);
-    serialCommand.addCommand("v1", api_v1);
-    serialCommand.addCommand("v2", api_v2);
-    serialCommand.addCommand("v3", api_v3);
-    serialCommand.addCommand("v4", api_v4);
-    serialCommand.addCommand("stop", api_stop);
     serialCommand.addCommand("start", api_start);
+    serialCommand.addCommand("stop", api_stop);
+    serialCommand.addCommand("v", api_v);
     serialCommand.setDefaultHandler(api_unknown);
 }
 
 
 // ============================================================================
 // custom functions:
+
+void api_start()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        robot_initNode(i);
+    }
+}
+
+void api_stop()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        robot_quickStop(i);
+    }
+}
 
 void api_v()
 {
@@ -41,7 +53,7 @@ void api_v()
         char *value = serialCommand.next();
         if (value == NULL)
         {
-            Serial.println("!Not enough arguments");
+            Serial.println("!E01: Not enough arguments supplied.");
             return;
         }
         v[i] = atoi(value);
@@ -50,101 +62,5 @@ void api_v()
     for (int i = 0; i < 4; i++)
     {
         robot_setMotorSpeed(i, v[i]);
-    }
-}
-
-void api_v1()
-{
-    char *arg1 = serialCommand.next();
-
-    // No arguments -> show usage
-    if (arg1 == NULL)
-    {
-        Serial.println("#v1 [Steps / second]");
-        return;
-    }
-
-    // there are arguments (number of steps)
-    if (arg1 != NULL)
-    {
-        // Converts a char string to a long
-        int v = atoi(arg1);
-        robot_setMotorSpeed(1, v);
-    }
-}
-
-void api_v2()
-{
-    char *arg1 = serialCommand.next();
-
-    // No arguments -> show usage
-    if (arg1 == NULL)
-    {
-        Serial.println("#v2 [Steps / second]");
-        return;
-    }
-
-    // there are arguments (number of steps)
-    if (arg1 != NULL)
-    {
-        // Converts a char string to a long
-        int v = atoi(arg1);
-        robot_setMotorSpeed(2, v);
-    }
-}
-
-void api_v3()
-{
-    char *arg1 = serialCommand.next();
-
-    // No arguments -> show usage
-    if (arg1 == NULL)
-    {
-        Serial.println("#v3 [Steps / second]");
-        return;
-    }
-
-    // there are arguments (number of steps)
-    if (arg1 != NULL)
-    {
-        // Converts a char string to a long
-        int v = atoi(arg1);
-        robot_setMotorSpeed(3, v);
-    }
-}
-
-void api_v4()
-{
-    char *arg1 = serialCommand.next();
-
-    // No arguments -> show usage
-    if (arg1 == NULL)
-    {
-        Serial.println("#v4 [Steps / second]");
-        return;
-    }
-
-    // there are arguments (number of steps)
-    if (arg1 != NULL)
-    {
-        // Converts a char string to a long
-        int v = atoi(arg1);
-        robot_setMotorSpeed(4, v);
-    }
-}
-
-void api_stop()
-{
-    for (int i = 1; i < 5; i++)
-    {
-        robot_quickStop(i);
-    }
-}
-
-void api_start()
-{
-    for (int i = 1; i < 5; i++)
-    {
-        robot_initNode(i);
     }
 }
