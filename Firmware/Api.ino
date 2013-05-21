d85fd43d8fb894b082435c3ddd0cfd98d0b33151
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-05-21 13:48:31 +0200

    commands now start with an @

--- Firmware/Api.ino
+++ Firmware/Api.ino
@@ -17,11 +17,11 @@ void api_unknown(const char *command)
     Serial.println(command);
 }
 
-void api_init()
+void api_begin()
 {
-    serialCommand.addCommand("start", api_start);
-    serialCommand.addCommand("stop", api_stop);
-    serialCommand.addCommand("v", api_v);
+    serialCommand.addCommand("@start", api_start);
+    serialCommand.addCommand("@stop", api_stop);
+    serialCommand.addCommand("@v", api_v);
     serialCommand.setDefaultHandler(api_unknown);
 }
 
@@ -32,40 +32,40 @@ void api_init()
 /**
  * Start the engines
  *
- * Usage: "start"
+ * Usage: "@start"
  */
 void api_start()
 {
-    for (int i = 0; i < 4; i++)
+    for (int wheel = 0; wheel < 4; wheel++)
     {
-        robot_initNode(i);
+        robot_initWheel(wheel);
     }
 }
 
 /**
  * Perform a quick stop
  *
- * Usage: "stop"
+ * Usage: "@stop"
  */
 void api_stop()
 {
-    for (int i = 0; i < 4; i++)
+    for (int wheel = 0; wheel < 4; wheel++)
     {
-        robot_quickStop(i);
+        robot_quickStop(wheel);
     }
 }
 
 /**
  * Set the desired velocity for each robot wheel.
  *
- * Usage: "v [wheel 1] [wheel 2] [wheel 3] [wheel 4]"
+ * Usage: "@v [wheel 1] [wheel 2] [wheel 3] [wheel 4]"
  * Range: -25000...25000
  * Unit:  steps/second
  */
 void api_v()
 {
     int v[4] = {0, 0, 0, 0};
-    for (int i = 0; i < 4; i++)
+    for (int wheel = 0; wheel < 4; wheel++)
     {
         char *value = serialCommand.next();
         if (value == NULL)
@@ -73,11 +73,11 @@ void api_v()
             Serial.println("!E01: Not enough arguments supplied.");
             return;
         }
-        v[i] = atoi(value);
+        v[wheel] = atoi(value);
     }
 
-    for (int i = 0; i < 4; i++)
+    for (int wheel = 0; wheel < 4; wheel++)
     {
-        robot_setMotorSpeed(i, v[i]);
+        robot_setMotorSpeed(wheel, v[wheel]);
     }
 }
